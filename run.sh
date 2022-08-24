d248675ff5b830fb7bbb730eeea243c345035bed
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-08-24 16:11:29 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 mkdir -p logs
 cd /home/sergio/scripts/cinecolombia/
+find logs -type f -mtime +30 -delete
 . ./env/bin/activate
 /home/sergio/scripts/cinecolombia/env/bin/python3 /home/sergio/scripts/cinecolombia/main.py >> logs/cinecolombia-"`date +"%Y-%m-%d_%H.%M.%S"`".log 2>&1
 deactivate
